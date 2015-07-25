8dbd950fa251df0211aa01469deeebfba9e4d7c9
pigmonchu <pigmonchu@Mac-Pro-de-pigmonchu.local>
2015-07-25 19:24:35 +0200

    Autorización

--- routes/index.js
+++ routes/index.js
@@ -20,11 +20,11 @@ router.get('/logout', sessionController.destroy);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 router.get('/author', quizController.author);
 
